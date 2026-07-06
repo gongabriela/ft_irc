617a61caa3c4bab764f0a420409fbf23dea215d4
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-07-06 14:59:37 +0100

    feat(auth): implement nickname validation and welcome reply

--- src/UserCommand.cpp
+++ src/UserCommand.cpp
@@ -27,8 +27,11 @@ std::vector<std::string> UserCommand::execute(Client& client, const ParsedComman
 
     std::cout << "[UserCommand] Client FD " << client.getFd() << " registered as " << cmd.args[0] << std::endl;
 
-    // TODO: verify if client has all required information (PASS, NICK, USER)
-    // If so, call client.setAuthenticated(true) and send welcome message (001)
-
+    if (!client.isAuthenticated() && client.hasPassword() && !client.getNickname().empty() && !client.getUsername().empty()) {
+        client.setAuthenticated(true);
+        replies.push_back(":server " RPL_WELCOME_CODE " " + client.getNickname() + " :" RPL_WELCOME_MSG + client.getNickname());
+        std::cout << "[Server] Client FD " << client.getFd() << " is now fully authenticated!" << std::endl;
+    }
+    // IRC Protocol does not require the server to reply in the case that the user is not authenticated correctly
     return replies;
 }
