ace63b230127381be677c93c56811ba56e3cc8f4
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-07-11 12:26:43 +0100

    fix: block unauth users from executing commands other than USER, NICK, PASS, and QUIT

--- include/Replies.hpp
+++ include/Replies.hpp
@@ -6,7 +6,7 @@
 /*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 15:22:27 by ggoncalv          #+#    #+#             */
-/*   Updated: 2026/07/07 17:21:59 by ggoncalv         ###   ########.fr       */
+/*   Updated: 2026/07/10 21:08:48 by ggoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,5 +56,8 @@
 
 #define RPL_ENDOFNAMES_CODE "366"
 #define RPL_ENDOFNAMES_MSG "End of /NAMES list"
+
+#define ERR_NOTREGISTERED_CODE "451"
+#define ERR_NOTREGISTERED_MSG "You have not registered"
 
 #endif
--- src/CommandHandler.cpp
+++ src/CommandHandler.cpp
@@ -6,7 +6,7 @@
 /*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 14:19:34 by ggoncalv          #+#    #+#             */
-/*   Updated: 2026/07/07 15:31:17 by ggoncalv         ###   ########.fr       */
+/*   Updated: 2026/07/10 21:12:54 by ggoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,12 @@ std::vector<std::string> CommandHandler::execute(Client& client, const ParsedCom
         return replies;
     }
 
+    if (!client.isAuthenticated() && cmd.command != "PASS" && cmd.command != "NICK" && cmd.command != "USER" && cmd.command != "QUIT") {
+        replies.push_back(_server.buildReply(ERR_NOTREGISTERED_CODE, client.getNickname().empty() ? "*" : client.getNickname(), ERR_NOTREGISTERED_MSG));
+        std::cout << "[Security] Blocked unauthenticated command: " << cmd.command << " from FD " << client.getFd() << std::endl;
+        return replies;
+    }
+    
     std::map<std::string, ICommand*>::iterator it = _commands.find(cmd.command);
     
     if (it != _commands.end()) {
