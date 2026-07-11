bc1eca7052e8944936086c8bf0eea16b040a6935
Gabriela Oliveira <gabriela.oliveira1031@gmail.com>
2026-07-11 12:58:03 +0100

    feat(replies): add reply messages for privmsg cmd

--- include/Replies.hpp
+++ include/Replies.hpp
@@ -6,7 +6,7 @@
 /*   By: ggoncalv <ggoncalv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 15:22:27 by ggoncalv          #+#    #+#             */
-/*   Updated: 2026/07/10 21:08:48 by ggoncalv         ###   ########.fr       */
+/*   Updated: 2026/07/11 12:27:53 by ggoncalv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,7 +57,21 @@
 #define RPL_ENDOFNAMES_CODE "366"
 #define RPL_ENDOFNAMES_MSG "End of /NAMES list"
 
+// === unauth error ===
 #define ERR_NOTREGISTERED_CODE "451"
 #define ERR_NOTREGISTERED_MSG "You have not registered"
+
+// === PRIVMSG ====
+#define ERR_NOSUCHNICK_CODE "401"
+#define ERR_NOSUCHNICK_MSG "No such nick/channel"
+
+#define ERR_CANNOTSENDTOCHAN_CODE "404"
+#define ERR_CANNOTSENDTOCHAN_MSG "Cannot send to channel"
+
+#define ERR_NORECIPIENT_CODE "411"
+#define ERR_NORECIPIENT_MSG "No recipient given (PRIVMSG)"
+
+#define ERR_NOTEXTTOSEND_CODE "412"
+#define ERR_NOTEXTTOSEND_MSG "No text to send"
 
 #endif
